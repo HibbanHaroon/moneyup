b264160fde1c13345b9f87a1b4c3a26350e30c75
HibbanHaroon <01-131202-027@student.bahria.edu.pk>
2023-05-20 13:19:18 +0500

    Home View set up

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -11,13 +12,86 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent, // Set the desired background color
-        elevation: 0.0,
-        automaticallyImplyLeading: true,
-        // Other app bar properties
+      backgroundColor: const Color(0xFF1E1E1E),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 50.0),
+              child: Stack(
+                alignment: Alignment.centerRight,
+                children: [
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Money ",
+                        style: TextStyle(
+                          fontFamily: 'SF Pro Display',
+                          fontStyle: FontStyle.normal,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 30,
+                          letterSpacing: -1,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        "Up",
+                        style: TextStyle(
+                          fontFamily: 'SF Pro Display',
+                          fontStyle: FontStyle.normal,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 30,
+                          letterSpacing: -1,
+                          color: Color(0xFF79F959),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: SvgPicture.asset('assets/search.svg'),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: SizedBox(
+                width: double.infinity,
+                height: 50.0,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Button action
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF3F3F46),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(52.5),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Cash in...",
+                        style: TextStyle(
+                          fontFamily: 'SF Pro Display',
+                          fontStyle: FontStyle.normal,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 18,
+                          color: Color(0xFFA1A1AA),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
-      body: const Text("Home Menu"),
     );
   }
 }
